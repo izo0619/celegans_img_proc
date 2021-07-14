c7dd897f481cff1986e5daff2c7294bdbd837389
izo0619 <isabelzhong30@gmail.com>
2021-07-14 16:33:03 -0700

    redid segmentation cleaning script

--- segmentation_correction/segmentation_cleaner_scikit.py
+++ segmentation_correction/segmentation_cleaner_scikit.py
@@ -1,12 +1,15 @@
+from urllib.request import OpenerDirector
 import matplotlib.pyplot as plt
-from skimage import data
+from skimage import data, segmentation, color
 from skimage.util import img_as_ubyte
 from skimage.io import imread
 from skimage.morphology import (erosion, dilation, opening, closing,  # noqa
-                                white_tophat)
+                                white_tophat, remove_small_objects,remove_small_holes )
 from skimage.morphology import black_tophat, skeletonize, convex_hull_image  # noqa
 from skimage.morphology import disk
 from skimage.util.dtype import img_as_uint  # noqa
+import numpy as np
+from scipy import ndimage
 
 
 def plot_comparison(original, filtered, filter_name):
@@ -21,19 +24,25 @@ def plot_comparison(original, filtered, filter_name):
     ax2.axis('off')
     plt.show()
 
-orig_img = imread('./segmentation_correction/sample_seg_small.tif')
+def set_diff2d(A, B):
+    nrows, ncols = A.shape
+    dtype={'names':['f{}'.format(i) for i in range(ncols)], 'formats':ncols * [A.dtype]}
+    C = np.setdiff1d(A.copy().view(dtype), B.copy().view(dtype))
+    return C
+
+orig_img = imread('./segmentation_correction/h21/h21_sample_seg_filtered2.tif')
 # orig_img = orig_img.transpose(1,0,2).reshape(130,-1)
 orig_img = img_as_uint(orig_img)
 # fig, ax = plt.subplots()
 # ax.imshow(orig_img, cmap=plt.cm.gray)
 # plt.show()
 
-footprint = disk(6)
-eroded = erosion(orig_img, footprint)
+footprint = disk(7)
 
+# eroded = erosion(orig_img, footprint)
 # plot_comparison(orig_img, eroded, 'erosion')
 
-dilated = dilation(orig_img,footprint)
+# dilated = dilation(orig_img,footprint)
 # plot_comparison(orig_img, dilated, 'dilation')
 
 opened = opening(orig_img,footprint)
@@ -42,7 +51,7 @@ opened = opening(orig_img,footprint)
 # open_erode = erosion(open_open, footprint)
 # erode_open_erode = opening(open_erode, footprint)
 
-closed = closing(orig_img,footprint)
+# closed = closing(orig_img,footprint)
 # plot_comparison(orig_img, closed, 'closing')
 
 # w_tophat = white_tophat(orig_img, footprint)
@@ -51,31 +60,70 @@ closed = closing(orig_img,footprint)
 # b_tophat = black_tophat(orig_img, footprint)
 # plot_comparison(orig_img, b_tophat, 'black tophat')
 
+light_layer = np.where(opened == 3)
+dark_layer = np.where(opened == 4)
+bg_layer = np.where(opened == 2)
+bound_layer = np.where(opened == 1)
 
-fig, (ax1, ax2, ax3,ax4,ax5) = plt.subplots(ncols=5, figsize=(16, 8), sharex=True,
+opened[opened == 1] = 1
+opened[opened == 2] = 0
+opened[opened == 3] = 0
+opened[opened == 4] = 0
+
+# opened = remove_small_objects(opened, 10)
+# opened = remove_small_holes(opened, 10)
+fill_worm = ndimage.binary_fill_holes(opened)
+fill_worm = opening(fill_worm, disk(5))
+
+dilated = dilation(fill_worm, disk(1))
+
+boundary = dilated.copy()
+boundary[fill_worm == 1] = 0
+
+raw_img = imread('./segmentation_correction/h21/h21_sample_seg_orig.tif')
+raw_img = color.gray2rgb(raw_img)
+raw_img[boundary == 1] = (255,0,0)
+# raw_img = color.label2rgb(boundary, raw_img, bg_color=(0,0,0))
+# print(np.unique(raw_img))
+# raw_img[boundary == 1] = 255
+
+fig, (ax1, ax2, ax3) = plt.subplots(ncols=3, figsize=(16, 8), sharex=True,
                                    sharey=True)
 ax1.imshow(orig_img, cmap=plt.cm.gray)
-ax1.set_title('original')
+ax1.set_title('segmented')
 ax1.axis('off')
-ax2.imshow(eroded, cmap=plt.cm.gray)
-ax2.set_title('erosion')
-ax2.axis('off')
+# ax2.imshow(eroded, cmap=plt.cm.gray)
+# ax2.set_title('erosion')
+# ax2.axis('off')
+# ax2.imshow(opened, cmap=plt.cm.gray)
+# ax2.set_title('opening')
+# ax2.axis('off')
 
 # ax2.imshow(opened, cmap=plt.cm.gray)
 # ax2.set_title('opening')
 # ax2.axis('off')
 
-ax3.imshow(dilated, cmap=plt.cm.gray)
-ax3.set_title('dilated')
-ax3.axis('off')
+# ax3.imshow(fill_worm, cmap=plt.cm.gray)
+# ax3.set_title('filled')
+# ax3.axis('off')
 # ax3.imshow(opening, cmap=plt.cm.gray)
 # ax3.set_title('opening')
 # ax3.axis('off')
 
-ax4.imshow(opened, cmap=plt.cm.gray)
-ax4.set_title('opening')
-ax4.axis('off')
-ax5.imshow(closed, cmap=plt.cm.gray)
-ax5.set_title('closing')
-ax5.axis('off')
+
+# ax4.imshow(opened, cmap=plt.cm.gray)
+# ax4.set_title('opening')
+# ax4.axis('off')
+# ax4.imshow(dilated, cmap=plt.cm.gray)
+# ax4.set_title('dilation')
+# ax4.axis('off')
+
+ax2.imshow(boundary.astype('uint8'), cmap=plt.cm.gray)
+ax2.set_title('boundary')
+ax2.axis('off')
+
+ax3.imshow(raw_img, cmap=plt.cm.gray)
+ax3.set_title('orig')
+ax3.axis('off')
+
 plt.show()
