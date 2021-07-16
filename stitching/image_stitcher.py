c7e4449bd214b9cea8e3f7e6fc0e1d62f14d51f1
izo0619 <isabelzhong30@gmail.com>
2021-07-15 17:39:50 -0700

    cleaned and commented sticher

--- stitching/image_stitcher.py
+++ stitching/image_stitcher.py
@@ -15,38 +15,50 @@ well_alphas = [chr(i) for i in range(ord('a'),ord('f')+1)]
 error_wells = {}
 
 # helper functions
+# horizontally concatenates two images
 def get_concat_h(im1, im2):
     dst = Image.new('I;16', (im1.width + im2.width, im1.height))
     dst.paste(im1, (0, 0))
     dst.paste(im2, (im1.width, 0))
     return dst
 
+# vertically concatenates two images
 def get_concat_v(im1, im2):
     dst = Image.new('I;16', (im1.width, im1.height + im2.height))
     dst.paste(im1, (0, 0))
     dst.paste(im2, (0, im1.height))
     return dst
 
+# stiches one well together
+# params: subdir - the subdirectory that contains the folder where the set of images lies in
+#         well - the letter of the well
+
+# this worked on a file flow like so:
+# - 10x_wormimages (root path)
+#     - p01-growth-H01-10X (sub directory)
+#         - TimePoint_1
+#             - p01-growth-H01-10X_A01_s1.tif
+#             - p01-growth-H01-10X_A01_s2.tif
+#             - ...
+#     - p02-growth-H02-10X (sub directory)
+#     - ...
 def stich_well(subdir, well):
     if os.path.isdir(os.path.join(root_path, subdir)):
         dest_dir = root_path + '/' + subdir + '/stiched_images/'
+        # creates a folder called stitched_images if it doesn't exist already
         if not os.path.exists(dest_dir):
             os.mkdir(dest_dir)
 
+        # set of images within the subdirectory that we are trying to stitch
         source_dir = os.path.join(root_path, subdir, 'TimePoint_1/')
-            
+        
         name_header = subdir[:19]
-
         try:
             print('running well ' + well + ' in subdir ' + subdir)
             row_imgs = []
+            # creates array of stitched rows of images
             for i in range(rows):
                 # set first image (image on the far left of the row)
-                # test_dir = glob.glob(source_dir + name_header + well + '0' + '?' + '_s' + str(i*cols+1) + ext)
-                # print(test_dir)
-                # for file in test_dir:
-                #     print(file)
-                # base_img = Image.open(source_dir + name_header + well + '03_s' + str(i*cols+1) + ext)
                 base_img = Image.open(glob.glob(source_dir + name_header + well.upper() + '0' + '?' + '_s' + str(i*cols+1) + ext)[0])
                 # loop through each column and add respective image
                 for j in range(1,cols):
@@ -65,7 +77,7 @@ def stich_well(subdir, well):
             print("error while running well " + well + " in hour " + subdir + '\n')
             print(e)
 
-
+# stiches all wells in the directory together
 def stich_all_in_dir():
     for subdir in directory_contents:
         if os.path.isdir(os.path.join(root_path, subdir)):
@@ -100,23 +112,18 @@ def stich_all_in_dir():
                 except Exception as e:
                     print("error while running well " + well + " in hour" + subdir)
                     print(e)
+                    # store all error wells in a dictionary, should look like this but haven't really tested this part
+                    # too much:
+                        # error_wells = {
+                        #     'p42-growth-H42-10X_Plate_3275': ['d', 'e', 'f'],
+                        #     'p43-growth-H43-10X_Plate_3277': ['e', 'f']}
                     error_wells.setdefault(subdir, []).append(well)
 
-# error_wells = {
-#     # 'p42-growth-H42-10X_Plate_3275': ['d', 'e', 'f'],
-#     # 'p43-growth-H43-10X_Plate_3277': ['e', 'f'],
-#     # 'p44-growth-H44-10X_Plate_3279': ['a', 'b', 'c', 'd', 'e', 'f'],
-#     # 'p46-growth-H46-10X_Plate_3283': ['a'],
-#     'p47-growth-H47-10X_Plate_3285': ['a', 'd', 'e', 'f'],
-#     'p48-growth-H48-10X_Plate_3287': ['a', 'b', 'c', 'd', 'e', 'f'],
-#     # 'p49-growth-H49-10X_Plate_3289' : ['a', 'b', 'c', 'd', 'e', 'f'],
-#     'p49-growth-H49-10X_Plate_3289' : ['a', 'd', 'e', 'f'],
-#     'p50-growth-H50-10X-2_Plate_3292': ['a', 'b', 'c', 'd', 'e', 'f'],
-#     'p50-growth-H50-10X_Plate_3291': ['a', 'b', 'e', 'f'],
-#     'p51-growth-H51-10X_Plate_3294': ['a', 'b', 'c', 'd', 'e', 'f']
-# }
 
-# for sd, w in error_wells.items():
-#     for well in w:
-#         stich_well(sd,well)
+# runs wells with errors
+def stitch_error_wells():
+    for sd, w in error_wells.items():
+        for well in w:
+            stich_well(sd,well)
+
 
