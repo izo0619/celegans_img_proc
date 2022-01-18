c1399b5b404a1dbf94144aab849f0dec43f19cca
izo0619 <isabelzhong30@gmail.com>
2022-01-18 14:18:27 -0600

    resolution tweaking

--- conformal_mapping/piecewise_example.py
+++ conformal_mapping/piecewise_example.py
@@ -85,71 +85,83 @@ def complex_dist(pt1, pt2):
 # plt.show()
 
 #### global gridlines
-length = 100 # length of whole object - long way
-width = 10 # width of whole object - short way
-num_pts = 10 # density of border pts
-num_boxes = 10
-box_length = length/num_boxes
-all_squares = np.array([])
+def calculate_global_error(num_boxes):
+    length = 200 # length of whole object - long way
+    width = 10 # width of whole object - short way
+    num_pts = 5 # density of border pts
+    res = 2
+    # num_boxes = 10
+    box_length = length/num_boxes
+    all_squares = np.array([])
 
-# create general boundary
-line_a = np.linspace(width/2,-width/2,num_pts, endpoint=False) * 1j
-line_b = np.linspace(0,length,num_pts, endpoint=False) + -(width/2)*1j
-line_c = length + np.linspace(-width/2,width/2,num_pts, endpoint=False) * 1j
-line_d = np.linspace(length,0,num_pts, endpoint=False) + (width/2)*1j
-L_pts = ((np.concatenate((line_a, line_b, line_c, line_d))))
-L_spline = Splinep.from_complex_list(L_pts)
-
-# create gridlines
-glines = []
-for i in np.linspace(0,length,num_pts):
-    for j in np.linspace(-width/2,width/2,num_pts):
-        coord = i + j*1j
-        glines.append(coord)
-        # np.append(glines, coord)
-glines = np.array(glines)
-
-# generates array of forward and reverse maps
-reverse_maps = []
-forward_maps = []
-for i in range(num_boxes):
-    line_a = box_length*i + np.linspace(width/2, -width/2,num_pts, endpoint=False) * 1j
-    line_b = np.linspace(box_length*i,box_length*(i+1),num_pts, endpoint=False) + (-width/2)*1j
-    line_c = box_length*(i+1) + np.linspace(-width/2, width/2, num_pts, endpoint=False) * 1j
-    line_d = np.linspace(box_length*(i+1), box_length*i,num_pts, endpoint=True) + (width/2)*1j
+    # create general boundary
+    line_a = np.linspace(width/2,-width/2,num_pts, endpoint=False) * 1j
+    line_b = np.linspace(0,length,num_pts, endpoint=False) + -(width/2)*1j
+    line_c = length + np.linspace(-width/2,width/2,num_pts, endpoint=False) * 1j
+    line_d = np.linspace(length,0,num_pts, endpoint=False) + (width/2)*1j
     L_pts = ((np.concatenate((line_a, line_b, line_c, line_d))))
     L_spline = Splinep.from_complex_list(L_pts)
 
-    conformalCenter = box_length*(i+0.5) + 0j
-    plt.plot(conformalCenter.real, conformalCenter.imag, marker='x', color='gray')
-    sm = SzMap(L_spline, conformalCenter)
-    S = Szego(L_spline, conformalCenter)
-    t = np.arange(1000)/1000.
-    L_zs_orig = L_spline(t)
-    L_zs_circle = np.exp(1.0j * S.theta(t))
-    # reverse map: circle -> shape 
-    s = aaa(L_zs_orig, L_zs_circle)
-    # forward map: shape -> circle 
-    r = aaa(L_zs_circle, L_zs_orig, tol=1e-7)
-    reverse_maps.append(s)
-    forward_maps.append(r)
-    all_squares = np.concatenate((all_squares, L_pts))
+    # create gridlines
+    glines = []
+    for i in np.linspace(0,length, int(length/res) + 1):
+        for j in np.linspace(-width/2,width/2, int(width/res + 1)):
+            coord = i + j*1j
+            glines.append(coord)
+            # np.append(glines, coord)
+    glines = np.array(glines)
 
-glines_remap = []
-global_error = 0
-for pt in glines:
-    box_num = int(pt.real // box_length)
-    if box_num == num_boxes: box_num -= 1
-    new_pt = forward_maps[box_num](pt) # forward map
-    new_pt = reverse_maps[box_num](new_pt) # reverse map
-    glines_remap.append(new_pt)
-    global_error += complex_dist(pt, new_pt)
+    # generates array of forward and reverse maps
+    reverse_maps = []
+    forward_maps = []
+    for i in range(num_boxes):
+        line_a = box_length*i + np.linspace(width/2, -width/2,num_pts, endpoint=False) * 1j
+        line_b = np.linspace(box_length*i,box_length*(i+1),num_pts, endpoint=False) + (-width/2)*1j
+        line_c = box_length*(i+1) + np.linspace(-width/2, width/2, num_pts, endpoint=False) * 1j
+        line_d = np.linspace(box_length*(i+1), box_length*i,num_pts, endpoint=True) + (width/2)*1j
+        L_pts = ((np.concatenate((line_a, line_b, line_c, line_d))))
+        L_spline = Splinep.from_complex_list(L_pts)
 
-glines_remap = np.array(glines_remap)
-global_error = global_error / len(glines)
+        conformalCenter = box_length*(i+0.5) + 0j
+        plt.plot(conformalCenter.real, conformalCenter.imag, marker='x', color='gray')
+        sm = SzMap(L_spline, conformalCenter)
+        S = Szego(L_spline, conformalCenter)
+        t = np.arange(1000)/1000.
+        L_zs_orig = L_spline(t)
+        L_zs_circle = np.exp(1.0j * S.theta(t))
+        # reverse map: circle -> shape 
+        s = aaa(L_zs_orig, L_zs_circle)
+        # forward map: shape -> circle 
+        r = aaa(L_zs_circle, L_zs_orig, tol=1e-7)
+        reverse_maps.append(s)
+        forward_maps.append(r)
+        all_squares = np.concatenate((all_squares, L_pts))
 
-# plt.plot(all_squares.real, all_squares.imag, color="gray", label="Boundary lines + conformal centers")
-# plt.scatter(glines.real, glines.imag, c='red', marker="D", label="Original gridlines")
-# plt.scatter(glines_remap.real, glines_remap.imag, c= 'green', marker="x", label="Remapped gridlines")
-# plt.legend()
-# plt.show()
+    glines_remap = []
+    global_error = 0
+    for pt in glines:
+        box_num = int(pt.real // box_length)
+        if box_num == num_boxes: box_num -= 1
+        new_pt = forward_maps[box_num](pt) # forward map
+        new_pt = reverse_maps[box_num](new_pt) # reverse map
+        glines_remap.append(new_pt)
+        global_error += complex_dist(pt, new_pt)
+
+    glines_remap = np.array(glines_remap)
+    global_error = global_error / len(glines)
+
+    ratio = length / (width * num_boxes)
+    # file.write(str(ratio) + "," + str(global_error))
+    # file.write("\n")
+    plt.plot(all_squares.real, all_squares.imag, color="gray", label="Boundary lines + conformal centers")
+    plt.scatter(glines.real, glines.imag, c='red', marker="D", label="Original gridlines")
+    plt.scatter(glines_remap.real, glines_remap.imag, c= 'green', marker="x", label="Remapped gridlines")
+    plt.legend()
+    plt.show()
+
+# to_run = [1,2,4,10,20,40,100, 200, 400]
+# file = open('global_error_results.txt', 'w')
+# for nb in to_run:
+#     calculate_global_error(nb)
+# file.close()
+calculate_global_error(10)
